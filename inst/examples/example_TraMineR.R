27fa2ac3303289d679972c6f8804502bc902e12c
timelyportfolio <kent.russell@timelyportfolio.com>
2015-07-02 11:36:11 -0500

    experiment with combining sunburst and TraMineR

---
+++ inst/examples/example_TraMineR.R
@@ -0,0 +1,34 @@
+library(cluster)
+library(TraMineR)
+library(sunburstR)
+library(pipeR)
+
+# use example from TraMineR vignette
+data("mvad")
+mvad.alphab <- c(
+  "employment", "FE", "HE", "joblessness",
+  "school", "training"
+)
+mvad.seq <- seqdef(mvad, 17:86, xtstep = 6, alphabet = mvad.alphab)
+
+# to make this work, we'll compress the sequences with seqdss
+#   could also aggregate with dply later
+seqtab( seqdss(mvad.seq), tlim = 0, format = "SPS" ) %>>%
+  attr("freq") %>>%
+  (
+    data.frame(
+      # appending "-end" is necessary for this to work
+      sequence = paste0(
+        gsub(
+          x = names(.$Freq)
+          , pattern = "(/[0-9]*)"
+          , replacement = ""
+          , perl = T
+        )
+        ,"-end"
+      )
+      ,freq = as.numeric(.$Freq)
+      ,stringsAsFactors = FALSE
+    )
+  ) %>>%
+  sunburst
