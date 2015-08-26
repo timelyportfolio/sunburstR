a154f60ceee9c165a90c214534d312e8570d156e
timelyportfolio <kent.russell@timelyportfolio.com>
2015-08-26 13:08:31 -0500

    add ngram example

---
+++ inst/examples/example_ngram.R
@@ -0,0 +1,48 @@
+#  use sunburst to analyze ngram data from Peter Norvig
+#    http://norvig.com/mayzner.html
+
+library(sunburstR)
+library(pipeR)
+
+#  read the csv data downloaded from the Google Fusion Table linked in the article
+ngrams2 <- read.csv("./inst/examples/ngrams2.csv", stringsAsFactors = FALSE)
+
+ngrams2 %>>%
+  #  let's look at ngrams at the start of a word, so columns 1 and 3
+  (.[,c(1,3)]) %>>%
+  #  split the ngrams into a sequence by splitting each letter and adding -
+  (
+    data.frame(
+      sequence = strsplit(.[,1],"") %>>%
+        lapply( function(ng){ paste0(ng,collapse = "-") } ) %>>%
+        unlist
+      ,freq = .[,2]
+      ,stringsAsFactors = FALSE
+    )
+  ) %>>%
+  sunburst
+
+library(htmltools)
+
+ngrams2 %>>%
+  (
+    lapply(
+      seq.int(3:ncol(.))
+      ,function(letpos){
+        (.[,c(1,letpos)]) %>>%
+          #  split the ngrams into a sequence by splitting each letter and adding -
+          (
+            data.frame(
+              sequence = strsplit(.[,1],"") %>>%
+                lapply( function(ng){ paste0(ng,collapse = "-") } ) %>>%
+                unlist
+              ,freq = .[,2]
+              ,stringsAsFactors = FALSE
+            )
+          ) %>>%
+          sunburst
+      }
+    )
+  ) %>>%
+  tagList %>>%
+  browsable
