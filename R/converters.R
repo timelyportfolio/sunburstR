ef33280d8468f47f86ab42bc39ddbc455bf92be7
timelyportfolio <kent.russell@timelyportfolio.com>
2017-06-13 11:04:29 -0500

    fix csv_to_hier to work with tibble

--- R/converters.R
+++ R/converters.R
@@ -2,10 +2,10 @@
 csv_to_hier <- function(csv) {
   df <- dplyr::bind_rows(
     lapply(
-      strsplit(as.character(csv[,1]), "-"),
+      strsplit(as.character(csv[[1]]), "-"),
       function(rw) data.frame(t(rw), stringsAsFactors = FALSE)
     )
   )
-  df$size = csv[,2]
+  df$size = csv[[2]]
   d3r::d3_nest(df, value_cols = "size")
 }
